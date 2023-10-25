c9ad2b5a7e5f293c1ea32d170d79993c2fd040a3
DanielBrMz <A01254805@tec.mx>
2023-10-25 12:29:23 -0700

    preparing login components

---
+++ lib/components/login_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_doctor/utils/config.dart';
+import 'package:pocket_doctor/utils/text.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -10,9 +12,37 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
+    Config().init(context);
+    // build login text field
     return Scaffold(
-        body: Center(
-      child: Text('Auth Page'),
-    ));
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(AppText.enText['welcome_text']!,
+                  style: const TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Config.spaceSmall,
+              Text(
+                AppText.enText['singIn_text']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
